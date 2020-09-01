e6ee9af6673aa34ac3544d19558a035a99040998
anas <anas3506606@gmail.com>
2020-09-02 00:40:33 +0300

    Updates

--- precache-manifest.722bb50b6fed185dc2ef00ebdd4ba492.js
+++ precache-manifest.722bb50b6fed185dc2ef00ebdd4ba492.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "45713c39f068e6c7502db28a0a17e371",
+    "revision": "8b7687bd8718a0cfe8488548185f345f",
     "url": "/Anas-Abo-moch-August-31-2020/index.html"
   },
   {
-    "revision": "9ac4685ea1ebe5834349",
+    "revision": "b9ddcbf9345fec98109c",
     "url": "/Anas-Abo-moch-August-31-2020/static/css/2.98f886d0.chunk.css"
   },
   {
-    "revision": "3912d75bd2fb95ec749e",
+    "revision": "0924f690477fe9356357",
     "url": "/Anas-Abo-moch-August-31-2020/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "9ac4685ea1ebe5834349",
-    "url": "/Anas-Abo-moch-August-31-2020/static/js/2.3bc9ff35.chunk.js"
+    "revision": "b9ddcbf9345fec98109c",
+    "url": "/Anas-Abo-moch-August-31-2020/static/js/2.4b5a8144.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/Anas-Abo-moch-August-31-2020/static/js/2.3bc9ff35.chunk.js.LICENSE.txt"
+    "url": "/Anas-Abo-moch-August-31-2020/static/js/2.4b5a8144.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3912d75bd2fb95ec749e",
-    "url": "/Anas-Abo-moch-August-31-2020/static/js/main.50d79989.chunk.js"
+    "revision": "0924f690477fe9356357",
+    "url": "/Anas-Abo-moch-August-31-2020/static/js/main.2ecd0c16.chunk.js"
   },
   {
     "revision": "bc3e331cbf9b9bdc1ff2",
